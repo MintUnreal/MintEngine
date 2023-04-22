eebbe74860fdfb3bb2370a5485072692e60a864e
MintUnreal <maksimko.sokolov@gmail.com>
2023-04-22 17:30:26 +0300

    Типа система координат, перспектива, движение камерой

--- MintEngine/MintEngine/Game.cs
+++ MintEngine/MintEngine/Game.cs
@@ -9,7 +9,7 @@ using MintEngine.Rendering.Bridges;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
-
+using OpenTK.Input;
 
 namespace MintEngine
 {
@@ -36,21 +36,71 @@ namespace MintEngine
         Texture texture1;
         Texture texture2;
 
-        private Stopwatch stopWatch;
+        //camera
+        Vector3 Position = new Vector3(0.0f, 0.0f, 3.0f);
+        Vector3 cameraTarget = Vector3.Zero;
+        Vector3 cameraDirection;
+        Vector3 up = Vector3.UnitY;
+        Vector3 cameraRight;
+        Vector3 cameraUp;
+
+        float speed = 2f;
+        float sensitivity = 0.1f;
+
+        Vector3 vfront = new Vector3(0.0f, 0.0f, -1.0f);
+        Vector3 vup = new Vector3(0.0f, 1.0f, 0.0f);
         public void Load(object sender, EventArgs e)
         {
-            stopWatch = new Stopwatch();
-            stopWatch.Start();
+            game.CursorVisible = false;
+            game.CursorGrabbed = true;
             game.VSync = VSyncMode.On;
             GL.Enable(EnableCap.Texture2D);
             GL.Enable(EnableCap.DepthTest);
             GL.Enable(EnableCap.Blend);
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
             float[] vertices = {
-                //Position          Texture coordinates
-             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
-             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+                -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+                 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+                -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+                -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
             };
             int[] indices =
             {
@@ -77,24 +127,20 @@ namespace MintEngine
             GL.Viewport(0, 0, game.Width, game.Height);
         }
 
-        private int fps = 0;
-        private float angle = 0;
-        public void Render(object sender, EventArgs e)
+        private Stopwatch stopwatch;
+        public void Render(object sender, FrameEventArgs e)
         {
-            if(stopWatch.ElapsedMilliseconds > 10)
-            {
-                angle++;
-                Matrix4 rotation = Matrix4.CreateRotationY(MathHelper.DegreesToRadians(angle)) * Matrix4.CreateRotationX(MathHelper.DegreesToRadians(angle)) * Matrix4.CreateRotationZ(MathHelper.DegreesToRadians(angle));
-                Matrix4 scale = Matrix4.CreateScale(0.5f, 0.5f, 0.5f);
-                trans = rotation * scale;
-            }
-            if (stopWatch.ElapsedMilliseconds > 1000)
-            {
-                Console.WriteLine("FPS: "+fps);
-                stopWatch.Restart();
-                fps = 0;
-            }
-            else fps++;
+            cameraDirection = Vector3.Normalize(Position - cameraTarget);
+            cameraRight = Vector3.Normalize(Vector3.Cross(up, cameraDirection));
+            cameraUp = Vector3.Cross(cameraDirection, cameraRight);
+            CameraInput(e);
+
+            Matrix4 rotX = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(stopwatch.ElapsedMilliseconds*0.01f));
+            Matrix4 rotY = Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(stopwatch.ElapsedMilliseconds*0.01f));
+            Matrix4 rotZ = Matrix4.CreateRotationZ((float)MathHelper.DegreesToRadians(stopwatch.ElapsedMilliseconds*0.01f));
+            Matrix4 model = rotX * rotY * rotZ;
+            Matrix4 view = Matrix4.LookAt(Position, Position + vfront, vup);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(60.0f),(float) game.Width/game.Height, 0.1f, 100.0f);
 
             GL.ClearColor(Color4.CornflowerBlue);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
@@ -103,11 +149,87 @@ namespace MintEngine
             shader.SetInt("texture1", 0);
             texture2.Use(TextureUnit.Texture1);
             shader.SetInt("texture2", 1);
-            shader.SetMat4("transform", trans);
+            shader.SetMat4("model", model);
+            shader.SetMat4("view", view);
+            shader.SetMat4("projection", projection);
             vao.Draw();
             
 
             game.SwapBuffers();
+        }
+
+        //mouse
+        Vector2 lastPos;
+        float yaw = 0;
+        float pitch = 0;
+        bool firstMove = true;
+        private void CameraInput(FrameEventArgs e)
+        {
+            MouseState mouse = Mouse.GetState();
+            if (firstMove) // this bool variable is initially set to true
+            {
+                lastPos = new Vector2(mouse.X, mouse.Y);
+                firstMove = false;
+            }
+            if (!game.Focused) return;
+
+
+            float deltaX = mouse.X - lastPos.X;
+            float deltaY = mouse.Y - lastPos.Y;
+            lastPos = new Vector2(mouse.X, mouse.Y);
+
+            yaw += deltaX * sensitivity;
+
+            if (pitch > 89.0f)
+            {
+                pitch = 89.0f;
+            }
+            else if (pitch < -89.0f)
+            {
+                pitch = -89.0f;
+            }
+            else
+            {
+                pitch -= deltaY * sensitivity;
+            }
+            vfront.X = (float)Math.Cos(MathHelper.DegreesToRadians(pitch)) * (float)Math.Cos(MathHelper.DegreesToRadians(yaw));
+            vfront.Y = (float)Math.Sin(MathHelper.DegreesToRadians(pitch));
+            vfront.Z = (float)Math.Cos(MathHelper.DegreesToRadians(pitch)) * (float)Math.Sin(MathHelper.DegreesToRadians(yaw));
+            vfront = Vector3.Normalize(vfront);
+
+            KeyboardState input = Keyboard.GetState();
+
+            //...
+
+            if (input.IsKeyDown(Key.W))
+            {
+                Position += vfront * speed * (float)e.Time; //Forward 
+            }
+
+            if (input.IsKeyDown(Key.S))
+            {
+                Position -= vfront * speed * (float)e.Time; //Backwards
+            }
+
+            if (input.IsKeyDown(Key.A))
+            {
+                Position -= Vector3.Normalize(Vector3.Cross(vfront, vup)) * speed * (float)e.Time; //Left
+            }
+
+            if (input.IsKeyDown(Key.D))
+            {
+                Position += Vector3.Normalize(Vector3.Cross(vfront, vup)) * speed * (float)e.Time; //Right
+            }
+
+            if (input.IsKeyDown(Key.Space))
+            {
+                Position += up * speed * (float)e.Time; //Up 
+            }
+
+            if (input.IsKeyDown(Key.LShift))
+            {
+                Position -= up * speed * (float)e.Time; //Down
+            }
         }
     }
 }
